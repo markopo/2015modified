e1c961f8c1fd8f482ee4f4b62e6d4783199d6958
Marko <marko_poikkimaki@hotmail.com>
2015-07-25 17:22:13 +0200

    acf custom image 1,2,3

--- content-page.php
+++ content-page.php
@@ -36,18 +36,86 @@
 
 </article><!-- #post-## -->
 
+<?php
+    /** Bild1 */
+    $acf_img1 = get_field('img1');
+    $acf_img_bildtext1 = get_field('img_bildtext1');
+    $acf_img_titel1 = get_field('img_titel1');
+?>
+
+<?php if($acf_img1 != false && $acf_img_bildtext1 != false && $acf_img_titel1 != false): ?>
+<div class="acf-custom-image hentry">
+    <figure>
+        <h3><?php echo $acf_img_titel1; ?></h3>
+        <a href="<?php echo $acf_img1['url'] ?>">
+        <img src="<?php echo $acf_img1['url'] ?>" alt="<?php echo $acf_img1['title'] ?>" title="<?php echo $acf_img1['title'] ?>" >
+        </a>
+        <figcaption>
+            <?php echo $acf_img_bildtext1; ?>
+        </figcaption>
+    </figure>
+</div>
+<?php endif; ?>
+
+<?php
+/** Bild2 */
+$acf_img2 = get_field('img2');
+$acf_img_bildtext2 = get_field('img_bildtext2');
+$acf_img_titel2 = get_field('img_titel2');
+?>
+
+<?php if($acf_img2 != false && $acf_img_bildtext2 != false && $acf_img_titel2 != false): ?>
+    <div class="acf-custom-image hentry">
+        <figure>
+            <h3><?php echo $acf_img_titel2; ?></h3>
+            <a href="<?php echo $acf_img2['url'] ?>">
+                <img src="<?php echo $acf_img2['url'] ?>" alt="<?php echo $acf_img2['title'] ?>" title="<?php echo $acf_img2['title'] ?>" >
+            </a>
+            <figcaption>
+                <?php echo $acf_img_bildtext2; ?>
+            </figcaption>
+        </figure>
+    </div>
+<?php endif; ?>
+
+
+<?php
+/** Bild3 */
+$acf_img3 = get_field('img3');
+$acf_img_bildtext3 = get_field('img_bildtext3');
+$acf_img_titel3 = get_field('img_titel3');
+?>
+
+<?php if($acf_img3 != false && $acf_img_bildtext3 != false && $acf_img_titel3 != false): ?>
+    <div class="acf-custom-image hentry">
+        <figure>
+            <h3><?php echo $acf_img_titel3; ?></h3>
+            <a href="<?php echo $acf_img3['url'] ?>">
+                <img src="<?php echo $acf_img3['url'] ?>" alt="<?php echo $acf_img3['title'] ?>" title="<?php echo $acf_img3['title'] ?>" >
+            </a>
+            <figcaption>
+                <?php echo $acf_img_bildtext3; ?>
+            </figcaption>
+        </figure>
+    </div>
+<?php endif; ?>
+
 
 <?php
     $acf_nyheter_text = get_field('nyheter_text');
 ?>
 
-<?php if(isset($acf_nyheter_text) || !empty($acf_nyheter_text)): ?>
+<?php if($acf_nyheter_text != false): ?>
 <div class="nyheter_text hentry">
   <p>
       <?php echo $acf_nyheter_text; ?>
   </p>
 </div>
 <?php endif; ?>
+
+
+
+
 
 
 
